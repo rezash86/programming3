10ed449b4d6e53bb641f97a382eb9b83bce31f8d
JAC-SERVERS\reza.shalchian <reza.shalchian@a.com>
2023-12-01 09:51:38 -0500

    session 5 - full crud

---
+++ springmvcproject/springmvcproject/src/main/java/com/jac/mvc/springmvcproject/controller/DemoSecurityController.java
@@ -0,0 +1,29 @@
+package com.jac.mvc.springmvcproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoSecurityController {
+
+    @GetMapping("/")
+    public String showHome(){
+        return "home";
+    }
+
+    @GetMapping("/showLoginPage")
+    public String showLoginPage(){
+        return "plain-login";
+    }
+
+    @GetMapping("/leaders")
+    public String showLeaders(){
+        return "leaders";
+    }
+
+    @GetMapping("/access-denied")
+    public String showAccessDenied() {
+
+        return "access-denied";
+    }
+}
